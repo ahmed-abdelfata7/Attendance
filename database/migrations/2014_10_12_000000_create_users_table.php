0c327c68495b7fa7692994b0ccb16a5cc7c632f2
EngAhmed <ahmed.m.web.dev@gmail.com>
2019-03-09 23:26:38 +0400

    check_in,check_out

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,6 +19,8 @@ class CreateUsersTable extends Migration
             $table->string('email')->unique();
             $table->string('password');
             $table->enum("role",['engineer','admin','developer']);
+            $table->boolean("check_in")->default(0);
+            $table->boolean("check_out")->default(0);
             $table->rememberToken();
             $table->timestamps();
         });
